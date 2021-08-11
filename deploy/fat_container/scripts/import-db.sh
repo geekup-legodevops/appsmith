78311654c6b4472cfbefc977278e947278585fa9
phat.ct <phat.ct@geekup.vn>
2021-08-11 09:08:07 +0700

    Add import-db.js to restore DB using mongorestore

--- deploy/fat_container/scripts/import-db.sh
+++ deploy/fat_container/scripts/import-db.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "Importing db"
 # @TODO:
+set -o allexport
+. /opt/appsmith/docker.env
+set +o allexport
+node import-db.js
 echo "Import DB done"
--- deploy/fat_container/utils/db-handler/import-db.js
+++ deploy/fat_container/utils/db-handler/import-db.js
@@ -1,22 +1,18 @@
 // Init function export mongodb
 var shell = require('shelljs')
-var mongojs = require('mongojs')
-var db = mongojs('username:password@localhost:27017/sampleDB')
-db.getCollectionNames(function (err, names) {
-  if (err) {
-    console.log(err)
-    process.exit(1)
-  }
-  names.forEach((name) => {
-    var cmd = 'mongoimport --db sampleDB --collection ' + name + ' --type json --file D:\\' + name + '.json'
-    console.log('executing: ' + cmd)
-    shell.exec(cmd, function (code, stdout, stderr) {
-      if (code != 0) {
-        console.error('Error: ' + code)
-        console.log('Program output:', stdout)
-        console.log('Program stderr:', stderr)
-      }
-    })
-  })
-  process.exit(0)
+
+const MONGO_HOST = process.env.MONGO_HOST
+const MONGO_USERNAME= process.env.MONGO_USERNAME
+const MONGO_PASSWORD= process.env.MONGO_PASSWORD
+const MONGO_DATABASE= process.env.MONGO_DATABASE
+const BACKUP_PATH = '/opt/appsmith/data/backup'
+
+const cmd = `mongorestore --uri='mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DATABASE}' --gzip --archive=${BACKUP_PATH}/data.archive`
+console.log('executing: ' + cmd)
+shell.exec(cmd, function (code, stdout, stderr) {
+	if (code != 0) {
+	console.error('Error: ' + code)
+	console.log('Program output:', stdout)
+	console.log('Program stderr:', stderr)
+	}
 })
