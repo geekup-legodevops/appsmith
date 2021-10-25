4a00f03df4f08cf0f6ccedceb8a3cf5f5f6e10ee
phat.ct <phat.ct@geekup.vn>
2021-10-25 08:49:05 +0700

    Disable mongoDB chart if Mongo URI include cluster protocol

--- deploy/docker/templates/netdata/mongodb.conf.sh
+++ deploy/docker/templates/netdata/mongodb.conf.sh
@@ -3,9 +3,15 @@
 set -o nounset
 MONGODB_URI="$1"
 
+MONGODB_PROTOCOL=$(echo "$MONGODB_URI" | grep -oP "[a-z+]+(?=\:\/\/)")
 MONGODB_PASSWORD=$(echo "$MONGODB_URI" | grep -oP "\w+(?=@)")
 MONGODB_HOST=$(echo "$MONGODB_URI" | grep -oP "[a-z0-9.]+(?=\/)")
 MONGODB_DATABASE=$(echo "$MONGODB_URI" | grep -oP "\w+(?!\S)")
+
+if ! [[ "$MONGODB_PROTOCOL" = "mongodb" ]]; then
+  echo "This protocol is not currently supported by Netdata"
+	exit 0
+fi
 
 cat <<EOF
 priority: 650
